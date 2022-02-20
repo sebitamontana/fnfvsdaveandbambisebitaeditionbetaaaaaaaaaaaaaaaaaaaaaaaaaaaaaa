98c16b228de87fd1bb23c2c1677bd2d4725cb495
sebitamontana <93792473+sebitamontana@users.noreply.github.com>
2022-02-20 13:35:46 -0300

    events alpha

---
+++ example_mods/custom_events/change stage.lua
@@ -0,0 +1,10 @@
+function onEvent(name, value1, value2)
+   
+    makeLuaSprite(value2, value1);
+    addLuaSprite(value2, false);
+    addGlitchEffect(value2, 2,1)
+    addGlitchEffect(value1, 2,1)  
+    
+    
+    
+    end
